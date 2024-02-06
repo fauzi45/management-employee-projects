5596cdd8cafa4eaf513c458c4c516818f6bb5939
fauzi45 <uzi.fajar45@gmail.com>
2024-02-06 11:20:13 +0700

    add employees migration and models

---
+++ server/models/employees.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Employees extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Employees.belongsTo(models.Departments,{
+        foreignKey: 'departmentId'
+      });
+    }
+  }
+  Employees.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    position: DataTypes.STRING,
+    departmentId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Employees',
+  });
+  return Employees;
+};
