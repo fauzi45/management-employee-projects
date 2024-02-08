53224787bc0ecb49d65f84eb67751532cbef6159
fauzi45 <fauzi45fajar@gmail.com>
2024-02-08 20:55:40 +0700

    add relation on projectmodels client

--- server/models/projects.js
+++ server/models/projects.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Projects.hasMany(models.TeamProjects, {
+        foreignKey: "projectId",
+      });
+
     }
   }
   Projects.init({
