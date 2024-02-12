f423e4900769b2c109a38a9a806a2e77a443f111
fauzi45 <uzi.fajar45@gmail.com>
2024-02-12 14:53:23 +0700

    fix: add imageurl on models project

--- server/migrations/20240208130618-create-projects.js
+++ server/migrations/20240208130618-create-projects.js
@@ -24,6 +24,9 @@ module.exports = {
       status: {
         type: Sequelize.BOOLEAN
       },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
